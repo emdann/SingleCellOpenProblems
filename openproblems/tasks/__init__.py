99b4930291f67f85af0b60e3a0cebe63f751875c
Daniel Burkhardt <burkhardt.d.b@gmail.com>
2021-03-30 12:04:45 -0400

    Fix dimred init in tasks/

--- openproblems/tasks/__init__.py
+++ openproblems/tasks/__init__.py
@@ -1,6 +1,6 @@
 from . import denoising
 from . import differential_abundance
-from . import dimensional_reduction
+from . import dimensionality_reduction
 from . import label_projection
 from . import multimodal_data_integration
 from . import regulatory_effect_prediction
