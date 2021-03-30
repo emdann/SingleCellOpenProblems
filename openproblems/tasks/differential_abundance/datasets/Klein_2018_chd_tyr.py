babe994e83d701aefa4b6cbe9c2dc9aa4a63b231
Emma Dann <ed6@sanger.ac.uk>
2021-03-30 15:57:46 +0000

    fix tasks init

--- openproblems/tasks/differential_abundance/datasets/Klein_2018_chd_tyr.py
+++ openproblems/tasks/differential_abundance/datasets/Klein_2018_chd_tyr.py
@@ -11,3 +11,22 @@ def Klein_2018_chd_tyr_data(test=False):
     simulate_treatment(adata, n_conditions=3, n_replicates=2)
 
     return adata
+
+@dataset("Chd/tyr CRISPR perturbation dataset - multiple simulations")
+def Klein_2018_chd_tyr_data_n_simulations(test=False, n_simulations=10):
+    # Load UMI data
+    adata = load_zebrafish_chd_tyr(test=test)
+    # Simulate experimental conditions with N different seeds
+    for i in range(n_simulations):
+        seed = 42 + i
+        simulate_treatment(adata, seed=seed, n_conditions=2, n_replicates=3)
+        obs_simulation = ["condition", "replicate", "sample"]
+        adata.obs.columns = ['{x}_seed{s}'.format(x=x, s=seed) if x in obs_simulation else x for x in adata.obs.columns]
+        adata.obsm["ground_truth_probability_seed{s}".format(s=seed)] = adata.obsm["ground_truth_probability"]
+        uns_simulation = ["conditions", "replicates", "samples", "n_replicates", "n_conditions", "n_samples"]
+        for u in uns_simulation:
+            adata.uns['{u}_seed{s}'.format(u=u, s=seed)] = adata.uns[u]
+        ## Save simulation params used for metrics
+        max_effect_size = (adata.obsm["ground_truth_probability_seed43"] - 0.5).max().max()
+        adata.uns["DA_simulation_params_seed{s}".format(s=seed)] = {"max_effect_size":max_effect_size}
+    return adata
--- openproblems/tasks/differential_abundance/datasets/__init__.py
+++ openproblems/tasks/differential_abundance/datasets/__init__.py
@@ -1,2 +1,3 @@
 # TODO: update
 from .Klein_2018_chd_tyr import Klein_2018_chd_tyr_data
+from .Klein_2018_chd_tyr import Klein_2018_chd_tyr_data_n_simulations
