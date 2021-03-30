ea05bd107b4687f1e596618e80381bb07126fc70
Emma Dann <ed6@sanger.ac.uk>
2021-03-30 20:36:49 +0000

    added miloR to methods

---
+++ openproblems/tasks/differential_abundance/methods/milo.R
@@ -0,0 +1,38 @@
+# Milo
+# @param sce SingleCellExperiment
+# @return sce SingleCellExperiment
+
+# Dependencies
+library(SingleCellExperiment)
+library(miloR)
+
+# Make design matrix
+sample_col = "sample"
+condition_col = "condition"
+design_df <- as.tibble(colData(sce)[c(sample_col, condition_col)]) %>%
+    distinct() %>%
+    column_to_rownames(sample_col)
+design <- formula(paste('~', condition_col, collapse = ' '))  
+
+## Build graph neighbourhoods
+k <- metadata(sce)$n_samples * 5
+d <- 30
+prop <- 0.1
+milo <- Milo(sce)
+milo <- buildGraph(milo, k=k, d=d, reduced.dim = "PCA")
+milo <- makeNhoods(milo, prop = prop, k=k, d=d, reduced_dims = "PCA")
+
+## Test DA
+milo <- countCells(milo, meta.data = data.frame(colData(milo)), sample=sample_col)
+milo <- calcNhoodDistance(milo, d=d, reduced.dim = "PCA")
+DA_results <- testNhoods(milo, design = design, design.df = design_df)
+
+## Convert nhood FC to single-cell probability
+nhood_p <- exp(DA_results$logFC) / (exp(DA_results$logFC) + 1) 
+cell_p_cond1 <- milo@nhoods %*% nhood_p
+cell_p_cond2 <- 1 - cell_p_cond1
+prob_mat <- cbind(cell_p_cond1, cell_p_cond2)
+reducedDim(sce, "probability_estimate") <- as.matrix(prob_mat)
+
+## Return
+sce
---
+++ openproblems/tasks/differential_abundance/methods/milo.py
@@ -0,0 +1,20 @@
+from ....tools.conversion import r_function
+from ....tools.decorators import method
+from ....tools.utils import check_version
+
+_milo = r_function("milo.R")
+
+@method(
+    method_name="Milo",
+    paper_name="Milo: differential abundance testing on single-cell data using k-NN graphs",
+    paper_url="https://www.biorxiv.org/content/10.1101/2020.11.23.393769v1",
+    paper_year=2020,
+    code_url="https://github.com/MarioniLab/miloR",
+    code_version=check_version("rpy2"),
+    image="openproblems-r-extras"
+)
+def run_milo(adata):
+    ## Prepare anndata for anndata2ri conversion
+    adata.obsm["ground_truth_probability"] = adata.obsm["ground_truth_probability"].values
+    adata = _milo(adata)
+    return(adata)
