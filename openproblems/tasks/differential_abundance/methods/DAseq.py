9870cf2800ffe060a3617f434014be74df703a05
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2021-03-31 11:51:45 -0400

    Add basic method framework to DAseq.py

--- openproblems/tasks/differential_abundance/methods/DAseq.py
+++ openproblems/tasks/differential_abundance/methods/DAseq.py
@@ -1,1 +1,21 @@
+from ....tools.conversion import r_function
+from ....tools.decorators import method
 
+
+@method(
+    method_name="DA-seq",
+    paper_name="Detection of differentially abundant cell"
+    "subpopulations discriminates biological states"
+    "in scRNA-seq data"
+    paper_url="https://www.biorxiv.org/content/10.1101/711929v3",
+    paper_year=2020,
+    code_url="https://github.com/KlugerLab/DAseq",
+    #code_version="N/A",
+    # image="openproblems-template-image" # only if required
+)
+
+_daseq = r_function("DAseq.R")
+
+def DAseq(adata):
+  adata = _daseq(adata)
+  return adata
