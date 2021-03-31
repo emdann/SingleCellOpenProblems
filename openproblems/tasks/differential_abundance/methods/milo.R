b4de1049f89e5afa41b729b96552904406826c31
Emma Dann <ed6@sanger.ac.uk>
2021-03-31 15:25:47 +0000

    updating libraries

--- openproblems/tasks/differential_abundance/methods/milo.R
+++ openproblems/tasks/differential_abundance/methods/milo.R
@@ -5,30 +5,31 @@
 # Dependencies
 library(SingleCellExperiment)
 library(miloR)
+library(tidyverse)
 
 # Make design matrix
-sample_col <- "sample"
-condition_col <- "condition"
+sample_col = "sample"
+condition_col = "condition"
 design_df <- as.tibble(colData(sce)[c(sample_col, condition_col)]) %>%
-  distinct() %>%
-  column_to_rownames(sample_col)
-design <- formula(paste("~", condition_col, collapse = " "))
+    distinct() %>%
+    column_to_rownames(sample_col)
+design <- formula(paste('~', condition_col, collapse = ' '))  
 
 ## Build graph neighbourhoods
 k <- metadata(sce)$n_samples * 5
 d <- 30
 prop <- 0.1
 milo <- Milo(sce)
-milo <- buildGraph(milo, k = k, d = d, reduced.dim = "PCA")
-milo <- makeNhoods(milo, prop = prop, k = k, d = d, reduced_dims = "PCA")
+milo <- buildGraph(milo, k=k, d=d, reduced.dim = "PCA")
+milo <- makeNhoods(milo, prop = prop, k=k, d=d, reduced_dims = "PCA")
 
 ## Test DA
-milo <- countCells(milo, meta.data = data.frame(colData(milo)), sample = sample_col)
-milo <- calcNhoodDistance(milo, d = d, reduced.dim = "PCA")
+milo <- countCells(milo, meta.data = data.frame(colData(milo)), sample=sample_col)
+milo <- calcNhoodDistance(milo, d=d, reduced.dim = "PCA")
 DA_results <- testNhoods(milo, design = design, design.df = design_df)
 
 ## Convert nhood FC to single-cell probability
-nhood_p <- exp(DA_results$logFC) / (exp(DA_results$logFC) + 1)
+nhood_p <- exp(DA_results$logFC) / (exp(DA_results$logFC) + 1) 
 cell_p_cond1 <- milo@nhoods %*% nhood_p
 cell_p_cond2 <- 1 - cell_p_cond1
 prob_mat <- cbind(cell_p_cond1, cell_p_cond2)
